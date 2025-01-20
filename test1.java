d40c819838206ab1d895a4f9e8bd756163afa97d
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2025-01-20 23:19:58 +0530

    inital commit

---
+++ test1.java
@@ -0,0 +1,20 @@
+class test1{
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Goodbye, World");
+        int mynum = 5;
+        float myfloat = 5.22f;
+        String fname = "Asad ";
+        String lname = "Chaudhary";
+        String fullname = fname+lname;
+        
+        System.out.println(mynum);
+        System.out.println(myfloat);
+        System.out.println(fname);
+        System.out.println("Fullname : " + fullname);
+
+
+
+    }
+}
+
